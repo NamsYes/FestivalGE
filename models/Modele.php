b92a587dd8aee7122a38b15554501c79cf65fb24
Sachachen <102303534+Sachachen@users.noreply.github.com>
2022-10-10 02:20:17 +0200

    Add files via upload

---
+++ models/Modele.php
@@ -0,0 +1,361 @@
+<?php
+function getbdd() {
+    $user = 'root';
+    $pswd = '';
+    $host = 'localhost';
+    $bdd = 'Festivalsport';
+    $dsn = "mysql:$host=localhost;dbname=$bdd";
+    $connexion = new PDO($dsn, $user, $pswd);
+    return $connexion;
+}
+
+function obtenirReqEtablissements() //VALIDE
+{
+   $req="select id, nom from Etablissement order by id";
+   return $req;
+}
+
+function obtenirReqEtablissementsOffrantChambres()
+{
+   $req="select id, nom, nombreChambresOffertes from Etablissement where 
+         nombreChambresOffertes!=0 order by id";
+   return $req;
+}
+
+function obtenirReqEtablissementsAyantChambresAttribuées()
+{
+   $req="select distinct id, nom, nombreChambresOffertes from Etablissement, 
+         Attribution where id = idEtab order by id";
+   return $req;
+}
+
+function obtenirDetailEtablissement($connexion, $id) //VALIDE
+{
+   $req="select * from Etablissement where id='$id'";
+   //$rsEtab=mysql_query($req, $connexion);
+   //return mysql_fetch_array($rsEtab);
+   $res = $connexion->query($req);
+   return $res->fetch(PDO::FETCH_ASSOC);
+}
+
+function supprimerEtablissement($connexion, $id)
+{
+   $req="delete from Etablissement where id='$id'";
+   $connexion->query($req);
+}
+ 
+function modifierEtablissement($connexion, $id, $nom, $adresseRue, $codePostal, 
+                               $ville, $tel, $adresseElectronique, $type, 
+                               $civiliteResponsable, $nomResponsable, 
+                               $prenomResponsable, $nombreChambresOffertes)
+{  
+   $nom=str_replace("'", "''", $nom);
+   $adresseRue=str_replace("'","''", $adresseRue);
+   $ville=str_replace("'","''", $ville);
+   $adresseElectronique=str_replace("'","''", $adresseElectronique);
+   $nomResponsable=str_replace("'","''", $nomResponsable);
+   $prenomResponsable=str_replace("'","''", $prenomResponsable);
+  
+   $req="update Etablissement set nom='$nom',adresseRue='$adresseRue',
+         codePostal='$codePostal',ville='$ville',tel='$tel',
+         adresseElectronique='$adresseElectronique',type='$type',
+         civiliteResponsable='$civiliteResponsable',nomResponsable=
+         '$nomResponsable',prenomResponsable='$prenomResponsable',
+         nombreChambresOffertes='$nombreChambresOffertes' where id='$id'";
+   
+   $connexion->exec($req);
+}
+
+function creerEtablissement($connexion, $id, $nom, $adresseRue, $codePostal, 
+                            $ville, $tel, $adresseElectronique, $type, 
+                            $civiliteResponsable, $nomResponsable, 
+                            $prenomResponsable, $nombreChambresOffertes)
+{ 
+   $nom=str_replace("'", "''", $nom);
+   $adresseRue=str_replace("'","''", $adresseRue);
+   $ville=str_replace("'","''", $ville);
+   $adresseElectronique=str_replace("'","''", $adresseElectronique);
+   $nomResponsable=str_replace("'","''", $nomResponsable);
+   $prenomResponsable=str_replace("'","''", $prenomResponsable);
+   
+   $req="insert into Etablissement values ('$id', '$nom', '$adresseRue', 
+         '$codePostal', '$ville', '$tel', '$adresseElectronique', '$type', 
+         '$civiliteResponsable', '$nomResponsable', '$prenomResponsable',
+         '$nombreChambresOffertes')";
+   
+   $connexion->exec($req);
+}
+
+
+function estUnIdEtablissement($connexion, $id)
+{
+   $req="select * from Etablissement where id='$id'";
+   $rsEtab=$connexion->query($req);
+   return $rsEtab->fetch(PDO::FETCH_ASSOC);
+}
+
+function estUnNomEtablissement($connexion, $mode, $id, $nom)
+{
+   $nom=str_replace("'", "''", $nom);
+   // S'il s'agit d'une création, on vérifie juste la non existence du nom sinon
+   // on vérifie la non existence d'un autre établissement (id!='$id') portant 
+   // le même nom
+   if ($mode=='C')
+   {
+      $req="select * from Etablissement where nom='$nom'";
+   }
+   else
+   {
+      $req="select * from Etablissement where nom='$nom' and id!='$id'";
+   }
+   $rsEtab= $connexion->query($req);
+   return $rsEtab->fetch(PDO::FETCH_ASSOC);
+}
+
+function obtenirNbEtab($connexion)
+{
+   $req="select count(*) as nombreEtab from Etablissement";
+   $rsEtab=$connexion->query($req);
+   $lgEtab=$rsEtab->fetch(PDO::FETCH_ASSOC);
+   return $lgEtab["nombreEtab"];
+}
+
+function obtenirNbEtabOffrantChambres($connexion)
+{
+   $req="select count(*) as nombreEtabOffrantChambres from Etablissement where 
+         nombreChambresOffertes!=0";
+   $rsEtabOffrantChambres = $connexion->query($req);
+   $lgEtabOffrantChambres = $rsEtabOffrantChambres->fetch(PDO::FETCH_ASSOC);
+   return $lgEtabOffrantChambres["nombreEtabOffrantChambres"];
+}
+
+// Retourne false si le nombre de chambres transmis est inférieur au nombre de 
+// chambres occupées pour l'établissement transmis 
+// Retourne true dans le cas contraire
+function estModifOffreCorrecte($connexion, $idEtab, $nombreChambres)
+{
+   $nbOccup=obtenirNbOccup($connexion, $idEtab);
+   return ($nombreChambres>=$nbOccup);
+}
+
+// FONCTIONS RELATIVES AUX GROUPES
+
+function obtenirReqIdNomGroupesAHeberger()
+{
+   $req="select id, nom, nombrePersonnes from Groupe where hebergement='O' order by id";
+   return $req;
+}
+
+function obtenirNomGroupe($connexion, $id)
+{
+   $req="select nom from Groupe where id='$id'";
+   $rsGroupe = $connexion->query($req);
+   $lgGroupe = $rsGroupe->fetch(PDO::FETCH_ASSOC);
+   return $lgGroupe["nom"];
+}
+
+// FONCTIONS RELATIVES AUX ATTRIBUTIONS
+
+// Teste la présence d'attributions pour l'établissement transmis    
+function existeAttributionsEtab($connexion, $id)
+{
+   $req="select * From Attribution where idEtab='$id'";
+   $rsAttrib= $connexion->query($req);
+   return $rsAttrib->fetch(PDO::FETCH_ASSOC);
+}
+
+// Retourne le nombre de chambres occupées pour l'id étab transmis
+function obtenirNbOccup($connexion, $idEtab)
+{
+   $req="select IFNULL(sum(nombreChambres), 0) as totalChambresOccup from
+        Attribution where idEtab='$idEtab'";
+   $rsOccup=$connexion->query($req);
+   $lgOccup=$rsOccup->fetch(PDO::FETCH_ASSOC);
+   return $lgOccup["totalChambresOccup"];
+}
+
+// Met à jour (suppression, modification ou ajout) l'attribution correspondant à
+// l'id étab et à l'id groupe transmis
+function modifierAttribChamb($connexion, $idEtab, $idGroupe, $nbChambres)
+{
+   $req="select count(*) as nombreAttribGroupe from Attribution where idEtab=
+        '$idEtab' and idGroupe='$idGroupe'";
+   $rsAttrib = $connexion->query($req);
+   $lgAttrib = $rsAttrib->fetch(PDO::FETCH_ASSOC);
+   if ($nbChambres==0)
+      $req="delete from Attribution where idEtab='$idEtab' and idGroupe='$idGroupe'";
+   else
+   {
+      if ($lgAttrib["nombreAttribGroupe"]!=0)
+         $req="update Attribution set nombreChambres=$nbChambres where idEtab=
+              '$idEtab' and idGroupe='$idGroupe'";
+      else
+         $req="insert into Attribution values('$idEtab','$idGroupe', $nbChambres)";
+   }
+   $connexion->exec($req);
+}
+
+// Retourne la requête permettant d'obtenir les id et noms des groupes affectés
+// dans l'établissement transmis
+function obtenirReqGroupesEtab($id)
+{
+   $req="select distinct id, nom, nombrePersonnes from Groupe, Attribution where 
+        Attribution.idGroupe=Groupe.id and idEtab='$id'";
+   return $req;
+}
+            
+// Retourne le nombre de chambres occupées par le groupe transmis pour l'id étab
+// et l'id groupe transmis
+function obtenirNbOccupGroupe($connexion, $idEtab, $idGroupe)
+{
+   $req="select nombreChambres From Attribution where idEtab='$idEtab'
+        and idGroupe='$idGroupe'";
+   $rsAttribGroupe=$connexion->query($req);
+   if ($lgAttribGroupe=$rsAttribGroupe->fetch(PDO::FETCH_ASSOC))
+      return $lgAttribGroupe["nombreChambres"];
+   else
+      return 0;
+}
+
+// FONCTIONS DE CONTRÔLE DE SAISIE
+
+// Si $tel a une longueur de 10 caractères et est de type entier, on 
+// considère qu'il s'agit d'un tel
+function estUnTel($tel)
+{
+   // Le code postal doit comporter 10 chiffres
+   return strlen($tel)== 10 && estEntier($tel);
+}
+
+// Si $codePostal a une longueur de 5 caractères et est de type entier, on 
+// considère qu'il s'agit d'un code postal
+function estUnCp($codePostal)
+{
+   // Le code postal doit comporter 5 chiffres
+   return strlen($codePostal)== 5 && estEntier($codePostal);
+}
+
+// Si la valeur transmise ne contient pas d'autres caractères que des chiffres, 
+// la fonction retourne vrai
+function estEntier($valeur)
+{
+   return !preg_match("%[^0-9]%", $valeur);
+}
+
+// Si la valeur transmise ne contient pas d'autres caractères que des chiffres  
+// et des lettres non accentuées, la fonction retourne vrai
+function estChiffresOuEtLettres($valeur)
+{
+   return !preg_match("%[^a-zA-Z0-9]%", $valeur);
+}
+
+// Fonction qui vérifie la saisie lors de la modification d'un établissement. 
+// Pour chaque champ non valide, un message est ajouté à la liste des erreurs
+function verifierDonneesEtabM($connexion, $id, $nom, $adresseRue, $codePostal, 
+                              $ville, $tel, $nomResponsable, $nombreChambresOffertes)
+{
+   if ($nom=="" || $adresseRue=="" || $codePostal=="" || $ville=="" || 
+       $tel=="" || $nomResponsable=="" || $nombreChambresOffertes=="")
+   {
+      ajouterErreur("Chaque champ suivi du caractère * est obligatoire");
+   }
+   if ($nom!="" && estUnNomEtablissement($connexion, 'M', $id, $nom))
+   {
+      ajouterErreur("L'établissement $nom existe déjà");
+   }
+   if ($codePostal!="" && !estUnCp($codePostal))
+   {
+      ajouterErreur("Le code postal doit comporter 5 chiffres");   
+   }
+   if ($nombreChambresOffertes!="" && (!estEntier($nombreChambresOffertes) ||
+       !estModifOffreCorrecte($connexion, $id, $nombreChambresOffertes)))
+   {
+      ajouterErreur
+      ("La valeur de l'offre est non entière ou inférieure aux attributions effectuées");
+   }
+   if ($tel != "" && !estUnTel($tel))
+   {
+      ajouterErreur("Le numero de telephone dois etre composé de 10 chiffres");
+   }
+}
+
+// Fonction qui vérifie la saisie lors de la création d'un établissement. 
+// Pour chaque champ non valide, un message est ajouté à la liste des erreurs
+function verifierDonneesEtabC($connexion, $id, $nom, $adresseRue, $codePostal, 
+                              $ville, $tel, $nomResponsable, $nombreChambresOffertes)
+{
+   if ($id=="" || $nom=="" || $adresseRue=="" || $codePostal=="" || $ville==""
+       || $tel=="" || $nomResponsable=="" || $nombreChambresOffertes=="")
+   {
+      ajouterErreur("Chaque champ suivi du caractère * est obligatoire");
+   }
+   if($id!="")
+   {
+      // Si l'id est constitué d'autres caractères que de lettres non accentuées 
+      // et de chiffres, une erreur est générée
+      if (!estChiffresOuEtLettres($id))
+      {
+         ajouterErreur
+         ("L'identifiant doit comporter uniquement des lettres non accentuées et des chiffres");
+      }
+      else
+      {
+         if (estUnIdEtablissement($connexion, $id))
+         {
+            ajouterErreur("L'établissement $id existe déjà");
+         }
+      }
+   }
+   if ($nom!="" && estUnNomEtablissement($connexion, 'C', $id, $nom))
+   {
+      ajouterErreur("L'établissement $nom existe déjà");
+   }
+   if ($codePostal!="" && !estUnCp($codePostal))
+   {
+      ajouterErreur("Le code postal doit comporter 5 chiffres");   
+   }
+   if ($nombreChambresOffertes!="" && !estEntier($nombreChambresOffertes)) 
+   {
+      ajouterErreur ("La valeur de l'offre doit être un entier");
+   }
+   if ($tel != "" && !estUnTel($tel))
+   {
+      ajouterErreur("Le numero de telephone dois etre composé de 10 chiffres");
+   }
+}
+
+// FONCTIONS DE GESTION DES ERREURS
+
+function ajouterErreur($msg)
+{
+   if (! isset($_REQUEST['erreurs']))
+      $_REQUEST['erreurs']=array();
+   $_REQUEST['erreurs'][]=$msg;
+}
+
+function nbErreurs()
+{
+   if (!isset($_REQUEST['erreurs']))
+   {
+	   return 0;
+	}
+	else
+	{
+	   return count($_REQUEST['erreurs']);
+	}
+}
+ 
+function afficherErreurs()
+{
+   echo '<div class="msgErreur">';
+   echo '<ul>';
+   foreach($_REQUEST['erreurs'] as $erreur)
+	{
+      echo "<li>$erreur</li>";
+	}
+   echo '</ul>';
+   echo '</div>';
+} 
+
+
+?>
