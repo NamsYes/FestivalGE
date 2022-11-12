4a2dbab4911f1df359442980269bbe366dc524f9
NamsYes <102303373+NamsYes@users.noreply.github.com>
2022-11-12 03:44:05 +0100

    Presque Done : Modif et Donner Attribution

--- vueDonnerNbChambres.php
+++ vueDonnerNbChambres.php
@@ -1,7 +1,6 @@
 <?php $nomtitre = 'Donner nb chambres'; ?>
 <?php ob_start(); ?>
 <?php
-include("models/Modele.php"); 
 
 $connexion=getbdd();
 // SÉLECTIONNER LE NOMBRE DE CHAMBRES SOUHAITÉES
@@ -9,33 +8,34 @@ $connexion=getbdd();
 $idEtab=$_REQUEST['idEtab'];
 $idGroupe=$_REQUEST['idGroupe'];
 $nbChambres=$_REQUEST['nbChambres'];
-
-echo "
-<form method='POST' action='vueModificationAttributions.php'>
-	<input type='hidden' value='validerModifAttrib' name='action'>
-   <input type='hidden' value='$idEtab' name='idEtab'>
-   <input type='hidden' value='$idGroupe' name='idGroupe'>";
-   $nomGroupe=obtenirNomGroupe($connexion, $idGroupe);
-   
-   echo "
-   <br><center><h5>Combien de chambres souhaitez-vous pour le 
-   groupe $nomGroupe dans cet établissement ?";
-   
-   echo "&nbsp;<select name='nbChambres'>";
-   for ($i=0; $i<=$nbChambres; $i++)
-   {
-      echo "<option>$i</option>";
-   }
-   echo "
-   </select></h5>
-   <input type='submit' value='Valider' name='valider'>&nbsp&nbsp&nbsp&nbsp
-   <input type='reset' value='Annuler' name='Annuler'><br><br>
-   <a href='vueModificationAttributions.php?action=demanderModifAttrib'>Retour</a>
-   </center>
-</form>";
+echo $nbChambres;
+echo $idGroupe;
 
 ?>
+<form method='POST' action='index.php?page=ModifAttribution'>
+	<input type='hidden' value='validerModifAttrib' name='action'>
+   <input type='hidden' value='$idEtab' name='idEtab'>
+   <input type='hidden' value='$idGroupe' name='idGroupe'>
+   
+   <?php $nomGroupe=obtenirNomGroupe($connexion, $idGroupe); ?>
+   
+
+   <br><center>
+      <h5>Combien de chambres souhaitez-vous pour le groupe <?=$nomGroupe?> dans cet établissement ?"&nbsp;
+   
+   <select name='nbChambres'>
+
+      <?php for ($i=0; $i<=$nbChambres; $i++) : ?>
+         <option> <?=$i?></option>
+      <?php endfor;?>
+   </select></h5>
+
+   <input type='submit' value='Valider' name='valider'> 
+   <input type='reset' value='Annuler' name='Annuler'><br><br>
+   <a href='index.php?page=ModifAttribution&'>Retour</a>
+   </center>
+</form>
+
 
 <?php $contenu =ob_get_clean(); ?>
-<?php require 'pageTemplate.php'; ?>
 <?php echo $contenu; ?>
